56c7d1caad4669b8ca1195a81c650baa0167f30d
Joanna Wang <joanna.j.wang@gmail.com>
2015-06-22 13:24:42 -0700

    working version of most spriting functions

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -1,6 +1,15 @@
 var path = require("path");
 var SpriteMap = require("./SpriteMap");
 var l = require("./Layout");
+var fs = require("fs");
+
+var getDataFileName = function(spritemapName) {
+  return spritemapName + ".json";
+}
+
+var getImageFileName = function(spritemapName) {
+  return spritemapName + ".png";
+}
 
 module.exports = function(eyeglass, sass) {
   var sassUtils = require("node-sass-utils")(sass);
@@ -15,19 +24,21 @@ module.exports = function(eyeglass, sass) {
 
       // create sprite map and return url
       // layout = sass map of layout style, alignment, spacing
+      // TODO check conversion from other units to px?
+      // returns spritemap name (name parameter)
       "sprite-map($name, $layout, $paths...)": function(name, layout, paths, done) {
         var name = name.getValue();
         var layout = sassUtils.castToJs(layout);
 
-        // TODO: check if spacing and alignment exist
-        var spacing = layout.get(sass.types.String("spacing")).getValue();
-        var alignment = layout.get(sass.types.String("alignment")).getValue();
-        var layout = layout.get(sass.types.String("layout")).getValue();
+        var spacing = layout.coerce.get("spacing").value;
+        var alignment = layout.coerce.get("alignment");
+        var layout = layout.coerce.get("layout");
 
-        var layoutOptions = {
-          alignment: alignment,
-          spacing: spacing
-        }
+        var layoutOptions = {};
+        if (spacing)
+          layoutOptions.spacing = spacing;
+        if (alignment)
+          layoutOptions.alignment = alignment;
 
         var imagePaths = [];
         for (var i = 0; i < paths.getLength(); i++)
@@ -41,39 +52,139 @@ module.exports = function(eyeglass, sass) {
 
           sm.pack(layoutStyle, spacing);
 
-          sm.saveData(null, function(err, data) {
+          sm.saveData(getDataFileName(name), function(err, data) {
             if (err) throw err;
             console.log('*  wrote sprite data');
           });
 
-          sm.createSpriteMap(null, function(err, spritemap) {
+          sm.createSpriteMap(getImageFileName(name), function(err, spritemap) {
             if (err) throw err;
             console.log('*  created spritemap yay');
           });
         });
 
-        done(sassUtils.castToSass(name + ".png"));
+        done(sassUtils.castToSass(name));
       },
-
-      // TODO: check if options exist
 
       // sprite-layout(horizontal, (spacing: 5px, alignment: bottom))
       // --> (layout: horizontal, spacing: 50px, alignment: bottom)
-
       "sprite-layout($layout, $options)": function(layout, options, done) {
         var options = sassUtils.castToJs(options);
-        var spacing = options.get(sass.types.String("spacing"));
-        var alignment = options.get(sass.types.String("alignment"));
+
+        var spacing = sassUtils.castToJs(options.coerce.get("spacing"));
+        // if (spacing) spacing = spacing.convertTo("px", "");
+        var alignment = options.coerce.get("alignment");
+
+        // TODO: check if options are valid
+
+        if (!alignment) {
+          if (layout.getValue() == "vertical")
+            alignment = "left";
+          else if (layout.getValue() == "horizontal")
+            alignment = "right";
+        }
 
         var layoutSettings = new sassUtils.SassJsMap();
-        layoutSettings.set(sass.types.String("layout"), layout);
-        layoutSettings.set(sass.types.String("spacing"), spacing);
-        layoutSettings.set(sass.types.String("alignment"), alignment);
+        layoutSettings.coerce.set("layout", layout);
+        if (spacing)
+          layoutSettings.coerce.set("spacing", spacing);
+        if (alignment)
+          layoutSettings.coerce.set("alignment", alignment);
 
         done(sassUtils.castToSass(layoutSettings));
       },
 
-      "sprite-list()": function() {
+      "sprite-list($spritemap)": function(spritemap, done) {
+        var filename = getDataFileName(spritemap.getValue());
+        var outputStr = "";
+
+        fs.readFile(filename, 'utf8', function(err, data) {
+          if (err) throw err;
+          data = JSON.parse(data);
+
+          for (var sprite in data) {
+            outputStr += data[sprite]["name"] + "\n";
+          }
+
+          done(sassUtils.castToSass(outputStr));
+        });
+      },
+
+      "sprite-url($spritemap)": function(spritemap, done) {
+        var url = getImageFileName(spritemap.getValue());
+        done(sassUtils.castToSass(url));
+      },
+
+      "sprite-position($spritemap, $spritename)": function(spritemap, spritename, done) {
+        var filename = getDataFileName(spritemap.getValue());
+
+        fs.readFile(filename, 'utf8', function(err, data) {
+          if (err) throw err;
+          data = JSON.parse(data);
+
+          var x = new sassUtils.SassDimension(data[spritename.getValue()]["origin_x"], "px");
+          var y = new sassUtils.SassDimension(data[spritename.getValue()]["origin_y"], "px");
+
+          output = sassUtils.castToSass([x, y]);
+          output.setSeparator(false);
+
+          done(output);
+        });
+      },
+
+      "sprite-position-x($spritemap, $spritename)": function(spritemap, spritename, done) {
+        var filename = getDataFileName(spritemap.getValue());
+
+        fs.readFile(filename, 'utf8', function(err, data) {
+          if (err) throw err;
+          data = JSON.parse(data);
+
+          var output = new sassUtils.SassDimension(data[spritename.getValue()]["origin_x"], "px");
+
+          done(sassUtils.castToSass(output));
+        });
+      },
+
+      "sprite-position-y($spritemap, $spritename)": function(spritemap, spritename, done) {
+        var filename = getDataFileName(spritemap.getValue());
+
+        fs.readFile(filename, 'utf8', function(err, data) {
+          if (err) throw err;
+          data = JSON.parse(data);
+
+          var output = new sassUtils.SassDimension(data[spritename.getValue()]["origin_y"], "px");
+
+          done(sassUtils.castToSass(output));
+        });
+      },
+
+      "sprite-width($spritemap, $spritename)": function(spritemap, spritename, done) {
+        var filename = getDataFileName(spritemap.getValue());
+
+        fs.readFile(filename, 'utf8', function(err, data) {
+          if (err) throw err;
+          data = JSON.parse(data);
+
+          var output = new sassUtils.SassDimension(data[spritename.getValue()]["width"], "px");
+
+          done(sassUtils.castToSass(output));
+        });
+      },
+
+      "sprite-height($spritemap, $spritename)": function(spritemap, spritename, done) {
+        var filename = getDataFileName(spritemap.getValue());
+
+        fs.readFile(filename, 'utf8', function(err, data) {
+          if (err) throw err;
+          data = JSON.parse(data);
+
+          var output = new sassUtils.SassDimension(data[spritename.getValue()]["height"], "px");
+
+          done(sassUtils.castToSass(output));
+        });
+      },
+
+      "sprite-identifier($spritemap, $spritename)": function(spritemap, spritename, done) {
 
       }
 
--- test/test_spriting.js
+++ test/test_spriting.js
@@ -15,9 +15,10 @@ describe("spriting module", function () {
 
   it("sprite-map() sanity check", function (done) {
     var options = {
-      data: ".test { foo: sprite-map('corgi', sprite-layout(horizontal, (spacing: 50px, alignment: bottom)), 'images/corgi', 'images/samoyed', 'images/corgi/oliver.jpg') }"
+      data: ".test { foo: sprite-map('corgi-spritemap', sprite-layout(horizontal, (spacing: 100px, "
+        + "alignment: bottom)), 'images/corgi', 'images/samoyed', 'images/corgi/oliver.jpg') }"
     };
-    var expectedOutput = ".test {\n  foo: corgi.png; }\n";
+    var expectedOutput = ".test {\n  foo: corgi-spritemap; }\n";
     testutils.assertCompiles(options, expectedOutput, done);
   });
 
@@ -25,25 +26,90 @@ describe("spriting module", function () {
     var options = {
       data: ".test { foo: sprite-layout(horizontal, (spacing: 50px, alignment: bottom)) }"
     };
-    var expectedOutput = ".test {\n  foo: (layout: horizontal, spacing: 50px, alignment: bottom); }\n";
+    var expectedOutput = ".test {\n  foo: (layout: horizontal, spacing: 50px, alignment: bottom); }"
+    + "\n";
     testutils.assertCompiles(options, expectedOutput, done);
   });
 
-  // it("sprite-map() works with sprite-layout()", function (done) {
-  //   var options = {
-  //     data: ".test { foo: sprite-map('corgi', '-vr', 'images/corgi', 'images/samoyed', 'images/corgi/oliver.jpg') }"
-  //   };
-  //   var expectedOutput = ".test {\n  foo: corgi.png; }\n";
-  //   testutils.assertCompiles(options, expectedOutput, done);
-  // });
+  it("sprite-layout() check missing parameters", function (done) {
+    var options = {
+      data: ".test { foo: sprite-layout(horizontal, (alignment: bottom)) }"
+    };
+    var expectedOutput = ".test {\n  foo: (layout: horizontal, alignment: bottom); }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
 
-  // it("sprite-map() layout", function (done) {
-  //   var options = {
-  //     data: ".test { foo: sprite-map('corgi', '-vr', 'images/corgi', 'images/samoyed', 'images/corgi/oliver.jpg') }"
-  //   };
-  //   var expectedOutput = ".test {\n  foo: corgi.png; }\n";
-  //   testutils.assertCompiles(options, expectedOutput, done);
-  // });
+  it("sprite-list()", function (done) {
+    var options = {
+      data: ".test { foo: sprite-list('corgi-spritemap') }"
+    };
+    var expectedOutput = ".test {\n  foo: images/corgi/blurrycorgi.png images/corgi/corgiball.gif images/corgi/corgistampede.jpg images/corgi/oliver.jpg images/corgi/twocorgies.png images/samoyed/129500461375869254.jpg images/samoyed/il_fullxfull.331833603.jpg images/samoyed/Samoyed-3.jpg ; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
+
+  it("sprite-url()", function (done) {
+    var options = {
+      data: ".test { foo: sprite-url('corgi-spritemap') }"
+    };
+    var expectedOutput = ".test {\n  foo: corgi-spritemap.png; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
+
+  it("sprite-position()", function (done) {
+    var options = {
+      data: ".test { foo: sprite-position('corgi-spritemap', 'images/corgi/corgiball.gif') }"
+    };
+    var expectedOutput = ".test {\n  foo: 300px 925px; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
+
+  it("sprite-position-x()", function (done) {
+    var options = {
+      data: ".test { foo: sprite-position-x('corgi-spritemap', 'images/corgi/corgiball.gif') }"
+    };
+    var expectedOutput = ".test {\n  foo: 300px; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
+
+  it("sprite-position-y()", function (done) {
+    var options = {
+      data: ".test { foo: sprite-position-y('corgi-spritemap', 'images/corgi/corgiball.gif') }"
+    };
+    var expectedOutput = ".test {\n  foo: 925px; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
+
+  it("sprite-width()", function (done) {
+    var options = {
+      data: ".test { foo: sprite-width('corgi-spritemap', 'images/corgi/corgiball.gif') }"
+    };
+    var expectedOutput = ".test {\n  foo: 296px; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
+
+  it("sprite-height()", function (done) {
+    var options = {
+      data: ".test { foo: sprite-height('corgi-spritemap', 'images/corgi/corgiball.gif') }"
+    };
+    var expectedOutput = ".test {\n  foo: 200px; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
+
+  it("sprite-background mixin", function (done) {
+    var options = {
+      data: "@import 'spriting'; .test { @include sprite-background('corgi-spritemap'); }"
+    };
+    var expectedOutput = ".test {\n  background: url(\"corgi-spritemap.png\") no-repeat; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
+
+  it("sprite-position mixin", function (done) {
+    var options = {
+      data: "@import 'spriting'; .test { @include sprite-position('corgi-spritemap', 'images/corgi/corgiball.gif'); }"
+    };
+    var expectedOutput = ".test {\n  background-position: 300px 925px; }\n";
+    testutils.assertCompiles(options, expectedOutput, done);
+  });
 
 
 });
